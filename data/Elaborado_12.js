ff7de9136d63cdd45a55ac41f8735e6e9f04ac05
cadenadevalor <76475718+cadenadevalor@users.noreply.github.com>
2020-12-23 18:00:18 +0100

    Add files via upload

---
+++ data/Elaborado_12.js
@@ -0,0 +1,10 @@
+var json_Elaborado_12 = {
+"type": "FeatureCollection",
+"name": "Elaborado_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "77", "Nombre": "Licor y brandy Segarra", "Gestión": "Destilerías Julián Segarra", "Descrip": "Licores, absenta y brandy elaborados de forma artesanal por Julián Segarra.\n", "Dirección": "Sta. Isabel Reina de Hungría, 3", "Localidad": "Xert\n", "Producto": "BE-Bebidas (incluye espirituosas)", "Temporada": null, "Web Info": "http:\/\/juliansegarra.webatu.com\/destilator\/licor.htm" }, "geometry": { "type": "Point", "coordinates": [ 0.15908705958465, 40.51736115122133 ] } },
+{ "type": "Feature", "properties": { "Name": "169", "Nombre": "Fartons\n", "Gestión": "Famlia Polo", "Descrip": "Dulce de bollería alargado creado en los años 60 para acompañar a la Horchata. Creado a partir de la masa del panquemao se distingue por su forma alargada y por estar pintado y decorado con glasa.\n", "Dirección": "Polígono Industrial 3, Vimeters, 15,", "Localidad": "Alboraya", "Producto": "PP-Panadería, pastelería y dulces", "Temporada": "Todo el año\n", "Web Info": "http:\/\/www.fartonspolo.com\/" }, "geometry": { "type": "Point", "coordinates": [ -0.342560548644356, 39.499032086706109 ] } },
+{ "type": "Feature", "properties": { "Name": "170", "Nombre": "Pastissets de boniato", "Gestión": null, "Descrip": "Es el dulce por excelencia de la Navidad. La presentación habitual es en forma de pequeñas empanadillas y de ahí que se les nombre en diminutivo y plural.\n", "Dirección": "Ubicación representativa", "Localidad": "La elaboración de este dulce se realiza en numerosas comarcas de la Comunidad Valenciana", "Producto": "PP-Panadería, pastelería y dulces\n", "Temporada": "Invierno", "Web Info": null }, "geometry": { "type": "Point", "coordinates": [ 0.355079285149247, 40.46183769358656 ] } }
+]
+}
---
+++ data/PatrimonioNatural_4.js
@@ -0,0 +1,8 @@
+var json_PatrimonioNatural_4 = {
+"type": "FeatureCollection",
+"name": "PatrimonioNatural_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "192", "Nombre": "PARQUE NATURAL DE LA ALBUFERA\n", "Gestión": "Generalitat Valenciana\n", "Descrip": "La Albufera de Valencia es una laguna costera somera  situada en la costa mediterránea al sur de la ciudad de Valencia. Su valor ecológico es muy grande ya que aquí se encuentran especies de gran valor ecológico en peligro de extinción.\n", "Dirección": "El Palmar\n", "Localidad": "València\n", "Producto": "FHC-Frutas, hortalizas, verduras y cereales;PMC-Pescados, moluscos y crustáceos\n", "Temporada": "Todo el año\n", "Web Info": null }, "geometry": { "type": "Point", "coordinates": [ -0.358927669144423, 39.340165122833049 ] } }
+]
+}
